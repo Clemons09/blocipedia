158f0ee4b4b714cc25c378ee11e7ca7f953493ca
Cody Lemons <codylemons@Codys-MacBook-Pro.local>
2018-10-24 14:43:16 -0600

    user story 3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  get 'welcome/about'
+  
+  resources :wikis
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get 'about' => 'welcome#about'
   root 'welcome#index'
 end
